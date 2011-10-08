8e67401d363884c6d6b84093c7d1abb9d1133fbf
Peter Norrhall <peter.norrhall@movlin.se>
2011-10-08 11:00:55 +0200

    Implemented BDD with ShouldMatchers

--- Hello2/testsrc/se/extenda/scala/ChipBDD.scala
+++ Hello2/testsrc/se/extenda/scala/ChipBDD.scala
@@ -1,11 +1,21 @@
 package se.extenda.scala
 
 import org.scalatest.Spec
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
 
-class ChipBDD extends Spec {
+class ChipBDD extends FlatSpec with ShouldMatchers {
 
-  describe("En Chop") {
-    it("ska returnera -1 om den ingående array är tom") (pending)
-    it("ska returnera -1 om det efterfrågade elementet inte finns") (pending)
+  var ch: Chop = _
+  
+  "En Chop med tom array" should "returnera -1" in {
+      ch = new Chop()
+      ch.chop(3, Array[Int]()) should be (-1)
+    }
+
+  it should ("ska returnera -1 om det efterfrågade elementet inte finns") in {
+      ch = new Chop()
+      ch.chop(3, Array[Int](1)) should be (-1)
   }
+  
 }
